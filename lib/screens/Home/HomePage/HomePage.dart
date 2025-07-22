b4fb23e6b59ddbf53f3226b0f6b5ea42c3447460
mrshelile <52761283+mrshelile@users.noreply.github.com>
2025-07-23 00:46:38 +0200

    emergency screan

--- lib/screens/Home/HomePage/HomePage.dart
+++ lib/screens/Home/HomePage/HomePage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:i_tour/screens/Home/Profile/Profile.dart';
 import 'package:i_tour/screens/Home/TrackingList/Trackinglist.dart';
 import 'package:i_tour/screens/Home/TransportCabs/TransportCabs.dart';
+import 'package:i_tour/screens/Home/emergency/Emergency.dart';
 import 'package:i_tour/screens/Home/WeatherForcast/WeatherForcast.dart';
 import 'package:i_tour/screens/Maps/Maps.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
